63e9e6ad96bfc07d644bda81a251a322881bd74b
srijaniiserprinceton <sbdas@princeton.edu>
2021-09-25 18:20:44 -0400

    Example using namedtuple works.

--- qdpy_jax/test_jax_class.py
+++ qdpy_jax/test_jax_class.py
@@ -1,28 +1,26 @@
 import jax.numpy as jnp
 import jax
+from collections import namedtuple
 
-class test_class():
-    def __init__(self, ell):
-        self.ell = ell
-
-    def create_m_arr(self, ell1):
-        print('Compiling ', self.ell)
-        m_arr = jnp.arange(-self.ell, self.ell+1)
-        return m_arr
-
+test_class = namedtuple('test_class', ['ell'])
     
-# creating instance of the class
-try_class = test_class(10)
+def create_m_arr(test_class):
+    print('Compiling ', test_class.ell)
+    m_arr = jnp.arange(-test_class.ell, test_class.ell+1)
+    return m_arr
+    # return 2 * test_class.ell
 
 # jitting function of the class
-_create_m_arr = jax.jit(try_class.create_m_arr, static_argnums=(0,))
+_create_m_arr = jax.jit(create_m_arr, static_argnums=(0,))
 
 for ell in range(10,20):
     print('Executing ', ell)
-    try_class.ell = ell
-    __ = _create_m_arr(ell).block_until_ready()
+    try_class = test_class(ell)
+    __ = _create_m_arr(try_class).block_until_ready()
+    # print(__)
 
 for ell in range(10,30):
     print('Executing ', ell)
-    try_class.ell = ell
-    __ = _create_m_arr(ell).block_until_ready()
+    try_class = test_class(ell)
+    __ = _create_m_arr(try_class).block_until_ready()
+    # print(__)
