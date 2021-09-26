db93ed493d2db23026cd809320a7b2f9a469d642
srijaniiserprinceton <sbdas@princeton.edu>
2021-09-25 20:51:26 -0400

    namedtuple_Cvec works!

--- qdpy_jax/namedtuple_Cvec.py
+++ qdpy_jax/namedtuple_Cvec.py
@@ -3,6 +3,7 @@ import time
 import jax
 import jax.numpy as jnp
 from collections import namedtuple
+import sys
 
 def jax_Omega(ell, N):
     """Computes Omega_N^\ell"""
@@ -31,13 +32,14 @@ def jax_gamma(ell):
     return jnp.sqrt((2*ell + 1)/4/jnp.pi)
 
 
-def jax_get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref):
+#def jax_get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref):
+def jax_get_Cvec(gvars, qdpt_mode, eigfuncs):
     """Computing the non-zero components of the submatrix"""
     # ell = jnp.minimum(ell1, ell2)
-    ell = ell1   # !!!!!!!!!!! a temporary fix. This needs to be taken care of
+    ell = qdpt_mode.ell1   # !!!!!!!!!!! a temporary fix. This needs to be taken care of
     m = jnp.arange(-ell, ell+1)
 
-    len_s = jnp.size(s_arr)
+    len_s = jnp.size(gvars.s_arr)
 
     wigvals = jnp.zeros((2*ell+1, len_s))
 
@@ -48,7 +50,8 @@ def jax_get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref):
                       lambda i, wigvals: jax.ops.index_update(wigvals,jax.ops.index[:,i],1),
                       wigvals)
    
-    Tsr = jax_compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2)
+    # Tsr = jax_compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2)
+    Tsr = jax_compute_Tsr(gvars, qdpt_mode, eigfuncs)
     # -1 factor from definition of toroidal field                                                                                                                   
     '''wsr = np.loadtxt(f'{self.sup.gvar.datadir}/{WFNAME}')\                                                                                                      
     [:, self.rmin_idx:self.rmax_idx] * (-1.0)'''
@@ -63,43 +66,44 @@ def jax_get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref):
     integrand = Tsr * wsr   # since U and V are scaled by sqrt(rho) * r                                                                                             
     
     #### TO BE REPLACED WITH SIMPSON #####
-    integral = jnp.trapz(integrand, axis=1, x=r)
+    integral = jnp.trapz(integrand, axis=1, x=gvars.r)
     
-    prod_gammas = jax_gamma(ell1) * jax_gamma(ell2) * jax_gamma(s_arr)
-    omegaref = omegaref
-    Cvec = jax_minus1pow_vec(m) * 8*jnp.pi * omegaref * (wigvals @ (prod_gammas * integral))
+    prod_gammas = jax_gamma(qdpt_mode.ell1) * jax_gamma(qdpt_mode.ell2) * jax_gamma(gvars.s_arr)
+    omegaref = qdpt_mode.omegaref
+    Cvec = jax_minus1pow_vec(m) * 8*jnp.pi * qdpt_mode.omegaref * (wigvals @ (prod_gammas * integral))
     
     return Cvec
 
-def jax_compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2):
+#def jax_compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2):
+def jax_compute_Tsr(gvars, qdpt_mode, eigfuncs): 
     """Computing the kernels which are used for obtaining the                                                                                                    
     submatrix elements.                                                                                                                                               
     """
+    Tsr = jnp.zeros((len(gvars.s_arr), len(gvars.r)))
     
-    Tsr = jnp.zeros((len(s_arr), len(r)))
+    L1sq = qdpt_mode.ell1*(qdpt_mode.ell1+1)
+    L2sq = qdpt_mode.ell2*(qdpt_mode.ell2+1)
+    Om1 = jax_Omega(qdpt_mode.ell1, 0)
+    Om2 = jax_Omega(qdpt_mode.ell2, 0)
     
-    L1sq = ell1*(ell1+1)
-    L2sq = ell2*(ell2+1)
-    Om1 = jax_Omega(ell1, 0)
-    Om2 = jax_Omega(ell2, 0)
+    U1, U2, V1, V2 = eigfuncs.U1, eigfuncs.U2, eigfuncs.V1, eigfuncs.V2
     
     for i in range(len(s_arr)):
-        s = s_arr[i]
+        s = gvars.s_arr[i]
         ls2fac = L1sq + L2sq - s*(s+1)
         eigfac = U2*V1 + V2*U1 - U1*U2 - 0.5*V1*V2*ls2fac
         # wigval = w3j(ell1, s, ell2, -1, 0, 1)
         # using some dummy number until we write the 
         # function for mapping wigner3js
         wigval = 1.0
-        Tsr_at_i = -(1 - jax_minus1pow(ell1 + ell2 + s)) * \
-                    Om1 * Om2 * wigval * eigfac / r
+        Tsr_at_i = -(1 - jax_minus1pow(qdpt_mode.ell1 + qdpt_mode.ell2 + s)) * \
+                   Om1 * Om2 * wigval * eigfac / gvars.r
         Tsr = jax.ops.index_update(Tsr, i, Tsr_at_i)
         
     return Tsr
 
-
 # parameters to be included in the global dictionary later?
-s_arr = np.array([1,3,5], dtype='int')
+s_arr = jnp.array([1,3,5], dtype='int32')
 
 rmin = 0.3
 rmax = 1.0
@@ -116,7 +120,7 @@ r = r[rmin_ind:rmax_ind]
 # the rotation profile
 wsr = np.loadtxt('w.dat')
 wsr = wsr[:,rmin_ind:rmax_ind]
-wsr = jnp.asarray(wsr)   # converting to device array once
+wsr = jnp.array(wsr)   # converting to device array once
 
 # using fixed modes (0,200)-(0,200) coupling for testing
 n1, n2 = 0, 0
@@ -131,105 +135,40 @@ V = np.loadtxt('V3672.dat')
 U = U[rmin_ind:rmax_ind]
 V = V[rmin_ind:rmax_ind]
 
+# converting numpy arrays to jax.numpy arrays
+r = jnp.array(r)
+U, V = jnp.array(U), jnp.array(V)
+
 U1, U2 = U, U
 V1, V2 = V, V
 
+# creating the named tuples
+GVAR = namedtuple('GVAR', 'r wsr s_arr')
+QDPT_MODE = namedtuple('QDPT_MODE', 'ell1 ell2 omegaref')
+EIGFUNCS = namedtuple('EIGFUNCS', 'U1 U2 V1 V2')
+
+# initializing namedtuples. This could be done from a separate file later
+gvars = GVAR(r, wsr, s_arr)
+qdpt_mode = QDPT_MODE(ell1, ell2, omegaref)
+eigfuncs = EIGFUNCS(U1, U2, V1, V2)
+
 Niter = 100
 
-# testing the Omega() function
-_Omega = jax.jit(jax_Omega)
-__ = _Omega(n1,ell1)
+# testing get_Cvec() function                                                                                                                                                           # declaring only qdpt_mode as static argument. It is critical to note that it is
+# better to avoid trying to declare namedtuples containing arrays to be static argument.
+# since for our problem, a changed array will be marked by a changed mode, it is better
+# to club the non-array info in a separate namedtuple than the array info. For example,
+# here, qdpt_mode has non-array info while eigfuncs have array info.
+_get_Cvec = jax.jit(jax_get_Cvec, static_argnums=(1,))
+# __ = _get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref)
+__ = _get_Cvec(gvars, qdpt_mode, eigfuncs)
 
 t1 = time.time()
-for __ in range(Niter): Omega_eval = jax_Omega(n1, ell1).block_until_ready()
+for __ in range(Niter): __ = jax_get_Cvec(gvars, qdpt_mode, eigfuncs).block_until_ready()
 t2 = time.time()
 
 t3 = time.time()
-for __ in range(Niter): Omega_eval = _Omega(n1, ell1).block_until_ready()
-t4 = time.time()
-
-print("Omega()")
-print("JIT version is faster by", (t2-t1)/(t4-t3)) 
-
-
-# testing the minus1pow() function
-_minus1pow = jax.jit(jax_minus1pow)
-__ = _minus1pow(29)
-
-t1 = time.time()
-for __ in range(Niter): minus1pow_eval = jax_minus1pow(29).block_until_ready()
-t2 = time.time()
-
-
-t3 = time.time()
-for __ in range(Niter): minus1pow_eval = _minus1pow(29).block_until_ready()
-t4 = time.time()
-
-print("minus1pow()")
-print("JIT version is faster by: ", (t2-t1)/(t4-t3)) 
-
-
-# testing the minus1pow_vec() function
-_minus1pow_vec = jax.jit(jax_minus1pow_vec)
-__ = _minus1pow_vec(jnp.arange(29))
-
-t1 = time.time()
-for __ in range(Niter): minus1pow_vec_eval = jax_minus1pow_vec(jnp.arange(29)).block_until_ready()
-t2 = time.time()
-
-
-t3 = time.time()
-for __ in range(Niter): minus1pow_vec_eval = _minus1pow_vec(jnp.arange(29)).block_until_ready()
-t4 = time.time()
-
-print("minus1pow_vec()")
-print("JIT version is faster by: ", (t2-t1)/(t4-t3)) 
-
-
-# testing the gamma() function
-_gamma = jax.jit(jax_gamma)
-__ = _gamma(29)
-
-t1 = time.time()
-for __ in range(Niter): minus1pow_eval = jax_gamma(29).block_until_ready()
-t2 = time.time()
-
-
-t3 = time.time()
-for __ in range(Niter): minus1pow_eval = _gamma(29).block_until_ready()
-t4 = time.time()
-
-print("gamma()")
-print("JIT version is faster by: ", (t2-t1)/(t4-t3)) 
-
-
-
-# testing compute_Tsr() function
-_compute_Tsr = jax.jit(jax_compute_Tsr)
-__ = _compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2)
-
-t1 = time.time()
-for __ in range(Niter): __ = jax_compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2).block_until_ready()
-t2 = time.time()
-
-t3 = time.time()
-for __ in range(Niter): __ = _compute_Tsr(ell1, ell2, s_arr, r, U1, U2, V1, V2).block_until_ready()
-t4 = time.time()
-
-print("compute_Tsr()")
-print("JIT version is faster by: ", (t2-t1)/(t4-t3)) 
-
-
-# testing get_Cvec() function                                                                                                                                                                           
-_get_Cvec = jax.jit(jax_get_Cvec, static_argnums=(0,1))
-__ = _get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref)
-
-t1 = time.time()
-for __ in range(Niter): __ = jax_get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref).block_until_ready()
-t2 = time.time()
-
-t3 = time.time()
-for __ in range(Niter): __ = _get_Cvec(ell1, ell2, s_arr, r, U1, U2, V1, V2, omegaref).block_until_ready()
+for __ in range(Niter): __ = _get_Cvec(gvars, qdpt_mode, eigfuncs).block_until_ready()
 t4 = time.time()
 
 print("get_Cvec()")
