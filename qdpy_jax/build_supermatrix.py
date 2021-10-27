5674a285f9435087b0267db9b6e5b1cc5adeb88b
Samarth G Kashyap <g.samarth@tifr.res.in>
2021-10-27 20:18:58 +0530

    BUGFIX: SUBMAT_DICT no longer has 0s. newBUG: if condition for updating submat fails

--- qdpy_jax/build_supermatrix.py
+++ qdpy_jax/build_supermatrix.py
@@ -27,23 +27,23 @@ def build_SUBMAT_INDICES(CNM_AND_NBS):
         def update_submat_ind_iy(iy, submat_tile_ind):
             submat_tile_ind = jax.ops.index_update(submat_tile_ind,
                                                    jax.ops.index[ix, iy, 0],
-                                                   sum_dim[ix, 0])
+                                                   sum_dim[iy, 0])
                                                     
             submat_tile_ind = jax.ops.index_update(submat_tile_ind,
                                                    jax.ops.index[ix, iy, 1],
-                                                   sum_dim[iy, 1])
+                                                   sum_dim[ix, 1])
             
             submat_tile_ind = jax.ops.index_update(submat_tile_ind,
                                                    jax.ops.index[ix, iy, 2],
-                                                   sum_dim[ix, 2])
+                                                   sum_dim[iy, 2])
             
             submat_tile_ind =  jax.ops.index_update(submat_tile_ind,
                                                     jax.ops.index[ix, iy, 3],
-                                                    sum_dim[iy, 3])
+                                                    sum_dim[ix, 3])
 
             return submat_tile_ind
 
-        submat_tile_int = foril(0, CNM_AND_NBS.dim_blocks,
+        submat_tile_ind = foril(0, CNM_AND_NBS.dim_blocks,
                                 update_submat_ind_iy, submat_tile_ind)
 
         return submat_tile_ind
@@ -74,14 +74,6 @@ def build_SUBMAT_INDICES(CNM_AND_NBS):
                                        jax.ops.index[i, 3],
                                        jnp.sum(dimY_submat[:i+1, 0]))
 
-    # sum_dim = np.zeros((CNM_AND_NBS.dim_blocks, 4), dtype='int32')
-    # for i in range(CNM_AND_NBS.dim_blocks):
-    #     sum_dim[i, 0] = dimX_submat[0, :i].sum()
-    #     sum_dim[i, 1] = dimY_submat[:i, 0].sum()
-    #     sum_dim[i, 2] = dimX_submat[0, :i+1].sum()
-    #     sum_dim[i, 3] = dimY_submat[:i+1, 0].sum()
-    # sum_dim = jnp.asarray(sum_dim)
-
     # creating the startx, startx, endx, endy for submatrices
     submat_tile_ind = np.zeros((CNM_AND_NBS.dim_blocks,
                                 CNM_AND_NBS.dim_blocks, 4), dtype='int32')
@@ -95,15 +87,10 @@ def build_SUBMAT_INDICES(CNM_AND_NBS):
                                         'starty',
                                         'endx',
                                         'endy'],
-                                       # (sum_dim[:, 0],
-                                       #  sum_dim[:, 1],
-                                       #  sum_dim[:, 2],
-                                       #  sum_dim[:, 3]))
                                        (submat_tile_ind[:, :, 0],
                                         submat_tile_ind[:, :, 1],
                                         submat_tile_ind[:, :, 2],
                                         submat_tile_ind[:, :, 3])) 
-    print(submat_tile_ind[:, :, 0])
     return SUBMAT_DICT
 
 
@@ -200,32 +187,62 @@ class build_supermatrix_functions:
                 endx, endy = SUBMAT_DICT.endx[i,ii], SUBMAT_DICT.endy[i,ii]
 
                 # creating the submatrix
+                print(f'startx = {startx}, starty = {starty}' +
+                      f' endx = {endx}, endy = {endy}')
                 submat = jnp.ones((endx-startx, endy-starty), dtype='float32')
-                print(submat.shape, submatdiag.shape)
 
                 dell = ell1 - ell2
                 absdell = np.abs(dell)
+                print(submat.shape, absdell, jnp.diag(submatdiag)[absdell:-absdell, :].shape)
                 #!!!!!!!!!!!!!!THIS NEEDS TO BE FIXED!!!!!!!!!!!!!!!!!!!!!#
-                def true_func(operands):
-                    submat = operands
-                    submat = jax.lax.cond(absdell == 0,
-                                          jnp.diag(submatdiag),
-                                          jax.ops.index_update(submat,
+                '''
+                def true_func(sm):
+                    print(f"inside true_func, absdell = {absdell}")
+                    sm = jax.lax.cond(absdell == 0,
+                                      lambda __: jnp.diag(submatdiag),
+                                      lambda __: jax.ops.index_update(sm,
                                                         jax.ops.index[absdell:-absdell, :],
-                                                        jnp.diag(submatdiag)))
-                    return submat
+                                                        jnp.diag(submatdiag)),
+                                          operand=None)
+                    return sm
 
-                def false_func(operands):
-                    submat = operands
-                    submat = jax.ops.index_update(submat,
-                                                  jax.ops.index[:, absdell:-absdell],
-                                                  jnp.diag(submatdiag))
-                    return submat
+                def false_func(sm):
+                    sm = jax.ops.index_update(sm,
+                                              jax.ops.index[:, absdell:-absdell],
+                                              jnp.diag(submatdiag),
+                                              operand=None)
+                    return sm
 
-                submat = jax.lax.cond(dell >= 0,
+ 
+                submat = jax.lax.cond(dell > 0,
                                       true_func,
                                       false_func,
-                                      operand=(submat))
+                                      operand=submat)
+                '''
+
+                def false_func(sm):
+                    def tfunc2(sm2):
+                        sm2 = jax.ops.index_update(sm2,
+                                                   jax.ops.index[absdell:-absdell, :],
+                                                   jnp.diag(submatdiag))
+                        return sm2
+
+                    def ffunc2(sm2):
+                        sm2 = jax.ops.index_update(sm2,
+                                                   jax.ops.index[:, absdell:-absdell],
+                                                   jnp.diag(submatdiag))
+                        return sm2
+
+                    sm = jax.lax.cond(dell > 0,
+                                      ffunc2,
+                                      tfunc2,
+                                      operand=sm)
+                    return sm
+
+                submat = jax.lax.cond(absdell == 0,
+                                      lambda submat: jnp.diag(submatdiag),
+                                      false_func,
+                                      operand=submat)
 
                 supmat = jax.ops.index_update(supmat,
                                               jax.ops.index[startx:endx, starty:endy],
@@ -241,113 +258,44 @@ class build_supermatrix_functions:
 
         return supmat
 
-'''
-class build_supermatrix_functions:
-    """Function that returns the function to calculate
-    the superMatrix prior to solving eigenvalue problem.
-    This function is spefic to the central multiplet. So,
-    CENMULT needs to be a static argument.
-    """
-    def __init__(self):
-        pass
 
-    def get_func2build_supermatrix(self):
-        def build_supermatrix(CNM_AND_NBS, SUBMAT_DICT):
-            """Function to assimilate all the neighbour info
-            and return the function to compute the SuperMatrix'
-            """
+if __name__ == "__main__":
+    GVARS = gvar_jax.GlobalVars()
+    GVARS_PATHS, GVARS_TR, GVARS_ST = GVARS.get_all_GVAR()
 
-            # building the submatrix dictionary
-            # SUBMAT_DICT = self.build_SUBMAT_DICT(CNM_AND_NBS)
+    # jitting various functions
+    get_namedtuple_for_cenmult_and_neighbours_ = \
+        gjit.gnool_jit(build_CENMULT_AND_NBS.get_namedtuple_for_cenmult_and_neighbours,
+                    static_array_argnums = (0, 1, 2))
 
-            # tiling supermatrix with submatrices
-            supmat = self.tile_submatrices(CNM_AND_NBS, SUBMAT_DICT)
-            return supmat
-        return build_supermatrix
+    # extracting the pruned parameters for multiplets of interest
+    nl_pruned, nl_idx_pruned, omega_pruned, wig_list, wig_idx_full = \
+        prune_multiplets.get_pruned_attributes(GVARS, GVARS_ST)
 
-    def tile_submatrices(self, CNM_AND_NBS, SUBMAT_DICT):
-        """Function to loop over the submatrix blocks and tile in the
-        submatrices into the supermatrix.
-        """
+    lm = load_multiplets.load_multiplets(GVARS, nl_pruned,
+                                        nl_idx_pruned,
+                                        omega_pruned)
 
-        supmat = jnp.zeros((CNM_AND_NBS.dim_super,
-                            CNM_AND_NBS.dim_super), dtype='float32')
+    GVARS_PRUNED_ST = jf.create_namedtuple('GVARS_ST',
+                                           ['s_arr',
+                                            'nl_all',
+                                            'nl_idx_pruned',
+                                            'omega_list',
+                                            'fwindow',
+                                            'OM',
+                                            'wig_idx_full'],
+                                           (GVARS_ST.s_arr,
+                                            lm.nl_pruned,
+                                            lm.nl_idx_pruned,
+                                            lm.omega_pruned,
+                                            GVARS_ST.fwindow,
+                                            GVARS_ST.OM,
+                                            wig_idx_full))
 
+    nmults = len(GVARS.n0_arr)
 
-        s_arr = jnp.array([1,3,5], dtype='int32')
-
-        rmin = 0.3
-        rmax = 1.0
-
-        r = np.loadtxt(f'{data_dir}/r.dat') # the radial grid
-
-        # finding the indices for rmin and rmax
-        rmin_ind = np.argmin(np.abs(r - rmin))
-        rmax_ind = np.argmin(np.abs(r - rmax)) + 1
-
-        # clipping radial grid
-        r = r[rmin_ind:rmax_ind]
-
-        # the rotation profile
-        wsr = np.loadtxt(f'{data_dir}/w.dat')
-        wsr = wsr[:,rmin_ind:rmax_ind]
-        wsr = jnp.array(wsr)   # converting to device array once
-
-        # finding omegaref
-        omegaref = 1
-
-        for i in range(CNM_AND_NBS.dim_blocks):
-            for ii in range(i, CNM_AND_NBS.dim_blocks):
-                #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                idx1 = CNM_AND_NBS.nl_nbs_idx[i]
-                idx2 = CNM_AND_NBS.nl_nbs_idx[ii]
-
-                U1 = np.loadtxt(f'{eig_dir}/U{idx1}.dat')
-                V1 = np.loadtxt(f'{eig_dir}/V{idx1}.dat')
-
-                U1 = U1[rmin_ind:rmax_ind]
-                V1 = V1[rmin_ind:rmax_ind]
-                U2, V2 = U1, V1
-
-                r = jnp.array(r)
-                U1, V1 = jnp.array(U1), jnp.array(V1)
-                U2, V2 = jnp.array(U2), jnp.array(V2)
-
-                ell1 = CNM_AND_NBS.nl_nbs[0, 1]
-                ell2 = CNM_AND_NBS.nl_nbs[1, 1]
-
-                # creating the named tuples
-                GVAR = namedtuple('GVAR', 'r wsr s_arr')
-                QDPT_MODE = namedtuple('QDPT_MODE', 'ell1 ell2 omegaref')
-                EIGFUNCS = namedtuple('EIGFUNCS', 'U1 U2 V1 V2')
-
-                # initializing namedtuples. This could be done from a separate file later
-                gvars = GVAR(r, wsr, s_arr)
-                qdpt_mode = QDPT_MODE(ell1, ell2, omegaref)
-                eigfuncs = EIGFUNCS(U1, U2, V1, V2)
-
-                get_submat = cvec.compute_submatrix(gvars)
-                submatdiag = get_submat.jax_get_Cvec()(qdpt_mode, eigfuncs)
-                #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                startx, starty = SUBMAT_DICT.startx[i,ii], SUBMAT_DICT.starty[i,ii]
-                endx, endy = SUBMAT_DICT.endx[i,ii], SUBMAT_DICT.endy[i,ii]
-
-                # creating the submatrix
-                submat = jnp.ones((endx-startx, endy-starty), dtype='float32')
-                supmat = jax.ops.index_update(supmat,
-                                              jax.ops.index[startx:endx,
-                                                            starty:endy],
-                                              submat)
-                supmat = jax.ops.index_update(supmat,
-                                              jax.ops.index[:100, :100],
-                                              jnp.diag(submatdiag[:100]))
-
-
-                # to avoid repeated filling of the central blocks
-                supmat = jax.lax.cond(abs(i-ii)>0,\
-                                      lambda __: jax.ops.index_update(supmat,
-                                        jax.ops.index[starty:endy, startx:endx],
-                                        jnp.transpose(jnp.conjugate(submat))),
-                                        lambda __: supmat, operand=None)
-        return supmat
-'''
+    n0, ell0 = GVARS.n0_arr[0], GVARS.ell0_arr[0]
+    CENMULT_AND_NBS = get_namedtuple_for_cenmult_and_neighbours_(n0, ell0, GVARS_ST)
+    CENMULT_AND_NBS = tu.tree_map(lambda x: np.array(x), CENMULT_AND_NBS)
+    SUBMAT_DICT = build_SUBMAT_INDICES(CENMULT_AND_NBS)
+    print(SUBMAT_DICT.startx)
