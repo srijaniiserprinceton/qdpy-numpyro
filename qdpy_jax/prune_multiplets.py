BUGFIX: wigner issue fixed: supermatrix computation seems to works (expect for the systematic error of 0.03%)

qdpy_jax/prune_multiplets.py
@@ -8,7 +8,6 @@ get_namedtuple_for_cenmult_and_neighbours_ = \
     gjit.gnool_jit(build_CENMULT_AND_NBS.get_namedtuple_for_cenmult_and_neighbours,
                    static_array_argnums = (0, 1, 2))
 
-
 # slices out the unique nl, nl_idx and omega from
 # from the arguments nl, omega which may contain repetitions
 def get_pruned_multiplets(nl, omega, nl_all):
@@ -33,8 +32,7 @@ def get_pruned_multiplets(nl, omega, nl_all):
 
 def get_pruned_attributes(GVARS, GVARS_ST):
     wig_list = []
-    idx1_list = []
-    idx2_list = []
+    wig_idx = []
     
     for i in range(len(GVARS.n0_arr)):
         n0, ell0 = GVARS.n0_arr[i], GVARS.ell0_arr[i]
@@ -48,20 +46,8 @@ def get_pruned_attributes(GVARS, GVARS_ST):
             nl_pruned = np.concatenate((nl_pruned, CENMULT_AND_NBS.nl_nbs), 0)
             omega_pruned = np.append(omega_pruned, CENMULT_AND_NBS.omega_nbs)
             
-        '''
-        wig_list, idx1_list, idx2_list = wigmap.get_wigners(CENMULT_AND_NBS.nl_nbs,
-                                                            wig_list, idx1_list,
-                                                            idx2_list)
-        '''
         wig_list, wig_idx = wigmap.get_wigners(CENMULT_AND_NBS.nl_nbs, 
-                                               wig_list, idx1_list,
-                                               idx2_list)
-
-    '''
-    wig_idx_full = np.zeros((len(wig_list), 2), dtype=np.int32)
-    wig_idx_full[:, 0] = idx1_list
-    wig_idx_full[:, 1] = idx2_list
-    '''
+                                               wig_list, wig_idx)
 
     # extracting the unique multiplets in the nl_pruned
     nl_pruned, nl_idx_pruned, omega_pruned = get_pruned_multiplets(nl_pruned,
@@ -78,6 +64,5 @@ def get_pruned_attributes(GVARS, GVARS_ST):
     sortind_wig_idx = np.argsort(wig_idx)
     wig_idx = wig_idx[sortind_wig_idx]
     wig_list = wig_list[sortind_wig_idx]
-    
+
     return nl_pruned, nl_idx_pruned, omega_pruned, wig_list, wig_idx
-    

qdpy_jax/wigner_map2.py
@@ -47,7 +47,7 @@ def w3j_vecm(l1, l2, l3, m1, m2, m3):
     return wigvals
 
 
-def get_wigners(nl_nbs, wig_list, idx1_list, idx2_list):
+def get_wigners(nl_nbs, wig_list, wig_idx):
     @np.vectorize
     def find_idx_fac(ell1, s, ell2, m):
         dell = abs(ell1 - ell2)
@@ -58,7 +58,10 @@ def get_wigners(nl_nbs, wig_list, idx1_list, idx2_list):
 
         if m < 0:
             fac = -1
-        return idx1, idx2, fac
+
+        max_ord_mag_idx1 = 5
+        wig_idx = idx2*(10**max_ord_mag_idx1) + idx1
+        return wig_idx, fac
 
     num_multiplets = nl_nbs.shape[0]
     num_blocks = int(num_multiplets**2)
@@ -77,27 +80,19 @@ def get_wigners(nl_nbs, wig_list, idx1_list, idx2_list):
                 if s < dell:
                     continue
 
-                idx1, idx2, fac = find_idx_fac(l1arr, s, l2arr, m)
+                widx, fac = find_idx_fac(l1arr, s, l2arr, m)
                 exists = True
 
                 try:
-                    _i1 = idx1_list.index(idx1[4])
-                    _i2 = idx2_list.index(idx2[4])
-                    mask1 = np.array(idx1_list) == idx1[4]
-                    mask2 = np.array(idx2_list) == idx2[4]
-                    exists = (mask1*mask2).sum().astype('bool')
+                    _i1 = wig_idx.index(widx)
+                    exists = True
                 except ValueError:
                     exists = False
 
                 if not exists:
                     wigvals = w3j_vecm(ell1, s, ell2, -m, 0*m, m)
-                    idx1_list.extend(list(idx1))
-                    idx2_list.extend(list(idx2))
                     wig_list.extend(list(wigvals))
-
-    # computing a unified index for the wigner
-    max_ord_mag_idx1 = 5
-    wig_idx = idx2_list*(10**max_ord_mag_idx1) + idx1_list
+                    wig_idx.extend(list(widx))
 
     return wig_list, wig_idx
 
@@ -141,7 +136,9 @@ def get_wig_from_pc(ell1, s, ell2, m):
     wigidx_local = jnp.searchsorted(wig_idx, idx)
     wig2 = fac * wig_list[wigidx_local]
     tv = np.isclose(wig1, wig2)
-    print(f'({ell1:4d} :{ell2:4d} :{m:4d}) wig-actual = {wig1:9.6f}: wig-read = {wig2:9.6f} - Match = {tv}')
+    print(f'({ell1:4d} :{ell2:4d} :{m:4d}) ' +
+          f'wig-actual = {wig1:9.6f}: wig-read = {wig2:9.6f}' +
+          f'- Match = {tv}')
     return wig1, wig2
 
 def compute_uniq_wigners(ell, s, ellp, m):