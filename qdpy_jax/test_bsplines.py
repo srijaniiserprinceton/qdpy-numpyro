de27f480dac6eef57b9c65cd178ad8a3aff21746
Samarth G Kashyap <g.samarth@tifr.res.in>
2021-11-06 21:39:55 +0530

    bsplines match with scipy

--- qdpy_jax/test_bsplines.py
+++ qdpy_jax/test_bsplines.py
@@ -1,29 +1,33 @@
-from qdpy_jax import bsplines as bsp_jax
+from qdpy_jax import bsplines as bsp_adams
 from scipy.interpolate import BSpline as bsp_scipy
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import jax
 import numpy as np
 
-_bsp_func_jax = jax.jit(bsp_jax.bspline1d)
+_bsp_func_jax = jax.jit(bsp_adams.bspline1d)
 
 if __name__ == "__main__":
     N = 1000
-    x = np.linspace(0, 5, N)
-    y = (x + 1.5*x*x)*np.sin(np.pi/2 + 2*np.pi*x/10.)
-
-    k = 3
-    t = np.array([0.1, 1.2, 2.3, 3.4, 4.5])
-    t, c, __ = interpolate.splrep(x, y, k=k, t=t)
+    k = 2
+    knotstart = 0.1
+    knotend = 4.5
+    startarr = np.ones(k+1)*knotstart
+    endarr = np.ones(k+1)*knotend
+    t = np.append(startarr, np.linspace(0.1, 4.5, 20))
+    t = np.append(t, endarr)
+    c = np.random.rand(len(t)-k-1)
+    x_new = np.linspace(t.min()+0.005, t.max()-0.005, N)
     bsp_func_scipy = bsp_scipy(t, c, k)
 
-    x_new = np.linspace(0.2, 4.4, N)
     y_scipy = bsp_func_scipy(x_new)
-    # y_jax = _bsp_func_jax(x_new, c, t, k)
 
     plt.figure()
-    plt.plot(x, y, 'k', label='true')
-    plt.plot(x_new, y_scipy, '--r', label='scipy')
+    # plt.plot(x, y, 'k', label='true')
+    plt.plot(x_new, y_scipy, 'k', label='scipy')
 
-    y_jax = bsp_jax.bspline1d(x_new, c[1:-k], t, k)
-    plt.plot(x_new, y_jax, '-.g', label='jax')
+    y_adams= bsp_adams.bspline1d(x_new, c, t, k)
+    # y_adams = _bsp_func_jax(x_new, c, t, k)
+    plt.plot(x_new, y_adams, '-.r', label='adams')
+    plt.legend()
+    plt.show()
