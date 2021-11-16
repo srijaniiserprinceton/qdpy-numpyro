eigvals_sigma added; nan bugfix in hypmat computation; pyrotry2 works

qdpy_jax/1walk1iter_sparse.py
@@ -49,25 +49,21 @@ def model():
        
     totalsum = jax.lax.fori_loop(0, nmults, loop_over_mults, totalsum)
     '''
-    for i in range(nmults):                                                                   
-        # building the entire hypermatrix                                                     
+    for i in range(nmults):
+        # building the entire hypermatrix
         hypmat =\
-                 build_hm_sparse.build_hypmat_w_c(noc_hypmat_all_sparse[i],                   
+                 build_hm_sparse.build_hypmat_w_c(noc_hypmat_all_sparse[i],
                                                   fixed_hypmat_all_sparse[i],
-                                                  GVARS.ctrl_arr_up, 
-                                                  nc, len_s)                          
+                                                  GVARS.ctrl_arr_up,
+                                                  nc, len_s)
         
-        # finding the eigenvalues of hypermatrix                                              
-        eigvals, __ = jnp.linalg.eigh(hypmat.todense())                                       
-        eigvalsum = jnp.sum(eigvals)                                                          
-        totalsum += eigvalsum #+ elementsum                                                   
-        
+        # finding the eigenvalues of hypermatrix
+        eigvals, __ = jnp.linalg.eigh(hypmat.todense())
+        eigvalsum = jnp.sum(eigvals)
+        totalsum += eigvalsum #+ elementsum
+
         # totalsum += jnp.sum(hypmat.todense())                                             
-    
     return totalsum                                                                       
-
-
-
 
 # jitting model
 model_ = jax.jit(model)

qdpy_jax/globalvars.py
@@ -47,7 +47,7 @@ class qdParams():
     # the radial orders present
     radial_orders = np.array([0])
     # the bounds on angular degree for each radial order
-    ell_bounds = np.array([[195, 195]])
+    ell_bounds = np.array([[200, 205]])
 
     rmin, rth, rmax = 0.0, 0.9, 1.2
     fwindow =  150.0 
@@ -122,7 +122,7 @@ class GlobalVars():
         self.fwindow = qdPars.fwindow
         self.wsr = -1.0*np.loadtxt(f'{self.datadir}/w_s/w.dat')
         # self.wsr = np.load(f'wsr-spline.npy')
-        # self.wsr_extend()
+        self.wsr_extend()
 
         # generating the multiplets which we will use
         load_from_file = False
@@ -130,7 +130,7 @@ class GlobalVars():
                                               qdPars.radial_orders,
                                               qdPars.ell_bounds)
         self.n0_arr, self.ell0_arr = n_arr, ell_arr
-        self.eigvals_true = self.get_eigvals_true()
+        self.eigvals_true, self.eigvals_sigma = self.get_eigvals_true()
 
         # rth = r threshold beyond which the profiles are updated. 
         self.rth = qdPars.rth
@@ -163,6 +163,7 @@ class GlobalVars():
         self.knot_arr, self.ctrl_arr_up = self.get_spline_full_r(which_ex='upex')         
         __, self.ctrl_arr_lo = self.get_spline_full_r(which_ex='loex')                    
         __, self.ctrl_arr_dpt = self.get_spline_full_r(which_ex=None)
+        self.nc_total = self.ctrl_arr_up.shape[1]
 
         # since we'll be varying them only beyond rth
         self.ctrl_arr_up = self.ctrl_arr_up[:, self.ctrl_ind_th:]
@@ -183,7 +184,7 @@ class GlobalVars():
         # creating the bsp_params to be used in precomputation
         # self.ctrl_arr has shape (s x num_ctrl_pts)
         self.nc = self.ctrl_arr_up.shape[1]
-        self.bsp_params = (self.nc, self.knot_arr, self.spl_deg)
+        self.bsp_params = (self.nc_total, self.knot_arr, self.spl_deg)
 
         # making the ctrl_arr_up > ctrl_arr_lo at each point
         ind_swap = np.greater(self.ctrl_arr_lo, self.ctrl_arr_up)
@@ -208,35 +209,47 @@ class GlobalVars():
                                         self.ctrl_arr_lo, self.knot_arr,
                                         self.rth_ind, self.spl_deg)
         '''
+        return None
 
     def get_eigvals_true(self):
         n0arr = self.n0_arr
         ell0arr = self.ell0_arr
         nmults = len(n0arr)
         eigvals_true = np.array([])
+        eigvals_sigma = np.array([])
         for i in range(nmults):
             m = np.arange(-ell0arr[i], ell0arr[i]+1)
-            eigvals_true = np.append(eigvals_true,
-                                     self.findfreq(ell0arr[i], n0arr[i], m)[0])
-        return eigvals_true
+            _eval, _esig, __ = self.findfreq(ell0arr[i], n0arr[i], m)
+            eigvals_true = np.append(eigvals_true, _eval)
+            eigvals_sigma = np.append(eigvals_sigma, _esig)
+        return eigvals_true, eigvals_sigma
 
-    # {{{ def findfreq_vecm(data, l, n, m):
+    # {{{ def findfreq(data, l, n, m):
     def findfreq(self, l, n, m):
         '''
         Find the eigenfrequency for a given (l, n, m)
         using the splitting coefficients
-
         Inputs: (data, l, n, m)
             data - array (hmi.6328.36)
             l - harmonic degree
             n - radial order
             m - azimuthal order
-
         Outputs: (nu_{nlm}, fwhm_{nl}, amp_{nl})
             nu_{nlm}    - eigenfrequency in microHz
             fwhm_{nl} - FWHM of the mode in microHz
             amp_{nl}    - Mode amplitude (A_{nl})
         '''
+        def compute_totsigma(sigmas, m, L):
+            coeffs = np.zeros_like(sigmas)
+            totsigma = np.zeros_like(m, dtype=np.float64)
+            lencoeffs = len(coeffs)
+            for i in range(lencoeffs):
+                coeff1 = np.zeros_like(sigmas)
+                coeff1[i] = 1
+                leg = legval(1.0*m/L, coeff1)*L*0.001
+                totsigma += (leg * sigmas[i])**2
+            return np.sqrt(totsigma)
+
         data = self.hmidata
         L = np.sqrt(l*(l+1))
         try:
@@ -250,11 +263,13 @@ class GlobalVars():
         mask0 = m == 0
         maskl = abs(m) >= l
         splits = np.append([0.0], data[modeindex, 12:48])
+        split_sigmas = np.append([0.0], data[modeindex, 49:85])
+        totsigma = compute_totsigma(split_sigmas, m, L)
         totsplit = legval(1.0*m/L, splits)*L*0.001
         totsplit[mask0] = 0
         amp[maskl] = 0
-        return nu + totsplit, fwhm, amp
-    # }}} findfreq_vecm(data, l, n, m)
+        return totsplit, totsigma, amp
+    # }}} findfreq(data, l, n, m)
 
 
     def get_all_GVAR(self):
@@ -336,7 +351,8 @@ class GlobalVars():
                                             'ctrl_arr_lo',
                                             'ctrl_arr_dpt',
                                             'knot_arr',
-                                            'eigvals_true'],
+                                            'eigvals_true',
+                                            'eigvals_sigma'],
                                            (self.r,
                                             self.r_spline,
                                             self.rth,
@@ -347,7 +363,8 @@ class GlobalVars():
                                             self.ctrl_arr_lo,
                                             self.ctrl_arr_dpt,
                                             self.knot_arr,
-                                            self.eigvals_true))
+                                            self.eigvals_true,
+                                            self.eigvals_sigma))
         return GVAR_TRACED
 
     def get_namedtuple_gvar_static(self):

qdpy_jax/pyrotry2.py
@@ -5,9 +5,9 @@ import time
 import sys
 
 import os
-num_chains = 3
-os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={num_chains} " +\
-                          "--xla_dump_to=/tmp/foo"
+num_chains = 1
+# os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={num_chains}"
+#+"--xla_dump_to=/tmp/foo"
 
 import jax
 import jax.numpy as jnp
@@ -15,27 +15,24 @@ import jax.tree_util as tu
 from jax import random, vmap
 
 # new package in jax.numpy
-from qdpy_jax import build_cenmult_and_nbs as build_CENMULT_AND_NBS 
-from qdpy_jax import build_supermatrix as build_supmat
 from qdpy_jax import globalvars as gvar_jax
-from qdpy_jax import load_multiplets
-from qdpy_jax import jax_functions as jf
-from qdpy_jax import wigner_map2 as wigmap
-from qdpy_jax import prune_multiplets
+from qdpy_jax import sparse_precompute as precompute
+from qdpy_jax import build_hypermatrix_sparse as build_hm_sparse
+from qdpy_jax import build_supermatrix as build_supmat
 
 # importing pyro related packages
 import numpyro
 from numpyro.diagnostics import hpdi
 import numpyro.distributions as dist
-from numpyro.infer import NUTS, MCMC
+from numpyro.infer import NUTS, MCMC, SA
 from numpyro import handlers
 
 
 from jax.config import config
+jax.config.update("jax_log_compiles", 1)
 jax.config.update('jax_platform_name', 'cpu')
 config.update('jax_enable_x64', True)
 numpyro.set_platform('cpu')
-
 
 import pickle
 def save_obj(obj, name):
@@ -46,13 +43,17 @@ def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
+GVARS = gvar_jax.GlobalVars()
+GVARS_PATHS, GVARS_TR, GVARS_ST = GVARS.get_all_GVAR()
+eigvals_true = jnp.asarray(GVARS_TR.eigvals_true)
+eigvals_sigma = jnp.asarray(GVARS_TR.eigvals_sigma)
 
-#========================================================================
+noc_hypmat_all_sparse, fixed_hypmat_all_sparse,\
+    ell0_nmults, omegaref_nmults = precompute.build_hypmat_all_cenmults()
 
-W1T = 1.
-W3T = 1.
-W5T = 1.
-
+nc = GVARS.nc
+len_s = len(GVARS.s_arr)
+nmults = len(GVARS.n0_arr)
 
 def model():
     # setting min and max value to be 0.1*true and 3.*true
@@ -71,109 +72,36 @@ def model():
                 jnp.array(c3_list),
                 jnp.array(c5_list)]
 
-    sigma = numpyro.sample('sigma', dist.Uniform(1e-3, 0.1))
     eig_sample = jnp.array([])
 
     for i in range(nmults):
-        n0, ell0 = GVARS.n0_arr[i], GVARS.ell0_arr[i]
-        CENMULT_AND_NBS = get_namedtuple_for_cenmult_and_neighbours(n0, ell0, GVARS_ST)
-        CENMULT_AND_NBS = jf.tree_map_CNM_AND_NBS(CENMULT_AND_NBS)
+        # building the entire hypermatrix
+        hypmat = build_hm_sparse.build_hypmat_w_c(noc_hypmat_all_sparse[i],
+                                                  fixed_hypmat_all_sparse[i],
+                                                  ctrl_arr, nc, len_s)
 
-        SUBMAT_DICT = build_SUBMAT_INDICES(CENMULT_AND_NBS)
-        SUBMAT_DICT = jf.tree_map_SUBMAT_DICT(SUBMAT_DICT)
+        ell0 = ell0_nmults[i]
+        omegaref = omegaref_nmults[i]
+        _eigval_mult = get_eigs(hypmat.todense())[:2*ell0+1]/2./omegaref
+        eig_sample = jnp.append(eig_sample, _eigval_mult*GVARS.OM*1e6)
 
-        supmatrix = build_supermatrix(CENMULT_AND_NBS,
-                                      SUBMAT_DICT,
-                                      GVARS_PRUNED_ST,
-                                      GVARS_PRUNED_TR,
-                                      ctrl_arr)
-        eig_sample = jnp.append(eig_sample,
-                                get_eigs(supmatrix)[:2*ell0+1]/2./CENMULT_AND_NBS.omega_nbs[0])
-
-    # eig_sample = numpyro.deterministic('eig', eig_mcmc_func(w1=w1, w3=w3, w5=w5))
-    return numpyro.sample('obs', dist.Normal(eig_sample, sigma), obs=eigvals_true)
+    return numpyro.factor('obs', dist.Normal(eig_sample,
+                                             eigvals_sigma).log_prob(eigvals_true))
 
 
 def get_eigs(mat):
     eigvals, eigvecs = jnp.linalg.eigh(mat)
     eigvals = build_supmat.eigval_sort_slice(eigvals, eigvecs)
     return eigvals
-#========================================================================
 
-eigvals_true = np.array([])
-GVARS = gvar_jax.GlobalVars()
-GVARS_PATHS, GVARS_TR, GVARS_ST = GVARS.get_all_GVAR()
-
-# jitting various functions
-get_namedtuple_for_cenmult_and_neighbours = build_CENMULT_AND_NBS.get_namedtuple_for_cenmult_and_neighbours
-
-build_SUBMAT_INDICES = build_supmat.build_SUBMAT_INDICES
-
-# initialzing the class instance for supermatrix computation
-build_supmat_funcs = build_supmat.build_supermatrix_functions()
-build_supermatrix = build_supmat_funcs.get_func2build_supermatrix()
-
-# extracting the pruned parameters for multiplets of interest
-nl_pruned, nl_idx_pruned, omega_pruned, wig_list, wig_idx =\
-                prune_multiplets.get_pruned_attributes(GVARS, GVARS_ST)
-
-lm = load_multiplets.load_multiplets(GVARS, nl_pruned,
-                                     nl_idx_pruned,
-                                     omega_pruned)
-
-GVARS_PRUNED_TR = jf.create_namedtuple('GVARS_TR',
-                                       ['r',
-                                        'r_spline',
-                                        'rth',
-                                        'wsr',
-                                        'U_arr',
-                                        'V_arr',
-                                        'wig_list',
-                                        'ctrl_arr_up',
-                                        'ctrl_arr_lo',
-                                        'knot_arr',
-                                        'eigvals_true'],
-                                       (GVARS_TR.r,
-                                        GVARS_TR.r_spline,
-                                        GVARS_TR.rth,
-                                        GVARS_TR.wsr,
-                                        lm.U_arr,
-                                        lm.V_arr,
-                                        wig_list,
-                                        GVARS_TR.ctrl_arr_up,
-                                        GVARS_TR.ctrl_arr_lo,
-                                        GVARS_TR.knot_arr,
-                                        GVARS_TR.eigvals_true))
-
-GVARS_PRUNED_ST = jf.create_namedtuple('GVARS_ST',
-                                       ['s_arr',
-                                        'nl_all',
-                                        'nl_idx_pruned',
-                                        'omega_list',
-                                        'fwindow',
-                                        'OM',
-                                        'wig_idx',
-                                        'rth_ind',
-                                        'spl_deg'],
-                                       (GVARS_ST.s_arr,
-                                        lm.nl_pruned,
-                                        lm.nl_idx_pruned,
-                                        lm.omega_pruned,
-                                        GVARS_ST.fwindow,
-                                        GVARS_ST.OM,
-                                        wig_idx,
-                                        GVARS_ST.rth_ind,
-                                        GVARS_ST.spl_deg))
-
-nmults = len(GVARS.n0_arr)
-eigvals_true = jnp.asarray(GVARS_TR.eigvals_true)
 
 # Start from this source of randomness. We will split keys for subsequent operations.
 rng_key = random.PRNGKey(12)
 rng_key, rng_key_ = random.split(rng_key)
 
 # Run NUTS.
-kernel = NUTS(model)
+#kernel = NUTS(model)
+kernel = SA(model)
 mcmc = MCMC(kernel, num_warmup=50, num_samples=100, num_chains=num_chains)
 mcmc.run(rng_key_)
 

qdpy_jax/sparse_precompute.py
@@ -48,13 +48,14 @@ def get_bsp_basis_elements(x):
         k = degree of the spline polynomials.
     """
 
-    nc, t, k = GVARS.bsp_params
+    nc_total, t, k = GVARS.bsp_params
+    nc_offset = nc_total - GVARS.nc
 
-    basis_elements = np.zeros((nc, len(x)))
+    basis_elements = np.zeros((GVARS.nc, len(x)))
     # looping over the basis elements for each control point
-    for c_ind in range(nc):
-        c = np.zeros(nc)
-        c[c_ind] = 1.0
+    for c_ind in range(GVARS.nc):
+        c = np.zeros(nc_total)
+        c[c_ind + nc_offset] = 1.0
         basis_elements[c_ind, :] = splev(x, (t, c, k))
     
     return basis_elements
@@ -163,6 +164,9 @@ def build_hypmat_all_cenmults():
 
     # storing as a list of sparse matrices
     noc_hypmat_all_sparse = []
+    ell0_nmults = []
+    omegaref_nmults = []
+    
 
     # the fixed hypat
     fixed_hypmat_all_sparse = []
@@ -170,9 +174,12 @@ def build_hypmat_all_cenmults():
     for i in range(nmults):
         # looping over all the central multiplets                                      
         n0, ell0 = GVARS.n0_arr[i], GVARS.ell0_arr[i]
+        ell0_nmults.append(ell0)
+
         # building the namedtuple for the central multiplet and its neighbours            
         CENMULT_AND_NBS = get_namedtuple_for_cenmult_and_neighbours(n0, ell0, GVARS_ST)
         SUBMAT_DICT = build_SUBMAT_INDICES(CENMULT_AND_NBS)
+        omegaref_nmults.append(CENMULT_AND_NBS.omega_nbs[0])
         
         noc_hypmat_this_s = []
         
@@ -203,7 +210,7 @@ def build_hypmat_all_cenmults():
         
     # list of shape (nmults x s x (nc x dim_hyper, dim_hyper))
     # the last bracket denotes matrices of that shape but in sparse form
-    return noc_hypmat_all_sparse, fixed_hypmat_all_sparse
+    return noc_hypmat_all_sparse, fixed_hypmat_all_sparse, ell0_nmults, omegaref_nmults
 
 def build_hm_nonint_n_fxd_1cnm(CNM_AND_NBS, SUBMAT_DICT, dim_hyper, s):
     '''Computes elements in the hypermatrix excluding the
@@ -275,20 +282,24 @@ def build_hm_nonint_n_fxd_1cnm(CNM_AND_NBS, SUBMAT_DICT, dim_hyper, s):
 
     # deleting wigvals 
     del wigvals
-            
+
     # making it a list to allow easy c * hypermat later
     for c_ind in range(GVARS.nc):
         # making sparse array
-        non_c_hypmat_arr_sparse =\
-                sparse.BCOO.fromdense(non_c_hypmat_arr[c_ind])
+        non_c_hypmat_UT = 0.5*np.diag(np.diag(non_c_hypmat_arr[c_ind]))
+        non_c_hypmat_UT += np.triu(non_c_hypmat_arr[c_ind], k=1)
+        non_c_hypmat = non_c_hypmat_UT + non_c_hypmat_UT.T
+        non_c_hypmat_arr_sparse = sparse.BCOO.fromdense(non_c_hypmat)
         non_c_hypmat_list.append(non_c_hypmat_arr_sparse)
 
     # deleting for ensuring no extra memory
     del non_c_hypmat_arr
     
     # sparsifying the fixed hypmat
+    fixed_hypmat_UT = 0.5*np.diag(np.diag(fixed_hypmat))
+    fixed_hypmat_UT += np.triu(fixed_hypmat, k=1)
+    fixed_hypmat = fixed_hypmat_UT + fixed_hypmat_UT.T
     fixed_hypmat_sparse = sparse.BCOO.fromdense(fixed_hypmat)
-    del fixed_hypmat
+    del fixed_hypmat, fixed_hypmat_UT
 
     return non_c_hypmat_list, fixed_hypmat_sparse
-