23a5848caf467afbf1b2e5849ff61b51c09e7494
srijaniiserprinceton <sbdas@princeton.edu>
2021-11-26 03:44:25 -0500

    run_reduced_problem_model.py now works via iterative inversion for c_3 and c_5

--- dpy_regression/run_reduced_problem_model.py
+++ dpy_regression/run_reduced_problem_model.py
@@ -1,24 +1,21 @@
-Bimport os
+B'''
+import os
 num_chains = 38
 os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={num_chains}"
+'''
 import numpy as np
 import jax
-print(jax.devices())
 from jax import random
 from jax import jit
+from jax import jacfwd, jacrev
 from jax.lax import fori_loop as foril
 from jax.lax import dynamic_slice as jdc
 from jax.lax import dynamic_update_slice as jdc_update
 from jax.ops import index as jidx
-import matplotlib.pyplot as plt
 import jax.numpy as jnp
-import numpyro
-import numpyro.distributions as dist
-from numpyro.infer import NUTS, MCMC, SA
 from jax.config import config
 config.update('jax_enable_x64', True)
 jidx_update = jax.ops.index_update
-import arviz as az
 import sys
 from dpy_jax import globalvars as gvar_jax
 
@@ -60,11 +57,13 @@ aconv_denom = np.zeros((nmults, Pjl.shape[1]))
 for mult_ind in range(nmults):
     aconv_denom[mult_ind] = np.diag(Pjl[mult_ind] @ Pjl[mult_ind].T)
 
-
 # number of s to fit
 len_s = true_params.shape[0]
 # number of c's to fit
 nc = true_params.shape[1]
+
+# number of data points 
+len_data = len(data)
 
 # converting to device array
 Pjl = jnp.asarray(Pjl)
@@ -125,30 +124,19 @@ np.testing.assert_array_almost_equal(pred_acoeffs, data_acoeffs)
 
 ######################################################
 
-num_params = len(cind_arr)
+# num_params = len(cind_arr)
 
-# setting the prior limits
-cmin = 0.5 * true_params / 1e-3
-cmax = 1.5 * true_params / 1e-3
-param_coeff *= 1e-3
+# flattening out the (s, nc) dimensions to easy dot product
+true_params = np.reshape(true_params, (nc * len_s))
+param_coeff = np.reshape(param_coeff, (nc * len_s, -1))
 
 
-def model():
+def loss_fn(c_arr):
     # predicted a-coefficients
     pred_acoeffs = jnp.zeros(num_j * nmults)
-    # sampling from a uniform prior
-    # c1 = []
-    c3 = []
-    c5 = []
 
-    for i in range(num_params):
-        c3.append(numpyro.sample(f'c3_{i}', dist.Uniform(cmin[0,i], cmax[0,i])))
-        c5.append(numpyro.sample(f'c5_{i}', dist.Uniform(cmin[1,i], cmax[1,i])))
-
-    c3 = jnp.asarray(c3)
-    c5 = jnp.asarray(c5)
-    
-    pred = fixed_part + c3 @ param_coeff[0] + c5 @ param_coeff[1]
+    # predicted frequency shifts
+    pred = fixed_part + c_arr @ param_coeff
     
     def loop_in_mults(mult_ind, pred_acoeff):
         pred_omega = jdc(pred, (mult_ind*dim_hyper,), (dim_hyper,))
@@ -160,61 +148,63 @@ def model():
     pred_acoeffs = foril(0, nmults, loop_in_mults, pred_acoeffs)
     misfit_acoeffs = (pred_acoeffs - data_acoeffs)/acoeffs_sigma
 
-    return numpyro.factor('obs', dist.Normal(0.0, 1.0).log_prob(misfit_acoeffs))
+    return jnp.mean(jnp.square(misfit_acoeffs))
+
+# function returning the hessian inverse
+def hessian(f):
+    return jacfwd(jacrev(f))
+
+# using autograd
+grad_fn = jax.grad(loss_fn)
+hess_fn = hessian(loss_fn)
+
+def update(c_arr, grads, hess_inv):
+    return jax.tree_multimap(lambda c, g, h: c - g @ h, c_arr, grads, hess_inv)
+
+# initial c_arr
+c_init = np.random.uniform(0.01*true_params, 2.0*true_params, size= nc*len_s)
+
+# the array to interate over
+c_arr = c_init.copy()
 
 
-def print_summary(samples, ctrl_arr):
-    keys = samples.keys()
-    for key in keys:
-        sample = samples[key]
-        key_split = key.split("_")
-        idx = int(key_split[-1])
-        sidx = int((int(key_split[0][1])-1)//2)
-        obs = ctrl_arr[sidx-1, idx] / 1e-3
-        print(f"[{obs:11.4e}] {key}: {sample.mean():.4e} +/- {sample.std():.4e}:" +
-              f"error/sigma = {(sample.mean()-obs)/sample.std():8.3f}")
+# max number of iterations
+Niter = 1000
+# initializing to some positive number
+loss = 10
+
+def cond_fun(func_args):
+    __, loss = func_args
+    return loss > 1e-10
+
+# iterating to solution
+def loop_iter(func_args):
+    c_arr, loss = func_args
+    # computing the misfit
+    loss = loss_fn(c_arr)
+    # computing the gradient
+    grads = grad_fn(c_arr)
+    # computing the hessian
+    hess = hess_fn(c_arr)
+    # computing the inverse of the hessian
+    hess_inv = jnp.linalg.inv(hess)
+    # stepping in ctrl array space
+    c_arr = update(c_arr, grads, hess_inv)
+
+    return c_arr, loss
+
+# jitting the loop functions
+loop_iter_ = jax.jit(loop_iter)    
+
+# while loop
+c_arr, loss = jax.lax.while_loop(cond_fun, loop_iter_, (c_arr, loss))
+
+
+def print_summary(inv_params, true_params):
+    for i in range(len(true_params)):
+        obs = true_params[i]
+        inv = inv_params[i]
+        print(f"[{obs:11.4e}] {inv:.4e}")
     return None
 
-
-# Start from this source of randomness. We will split keys for subsequent operations.    
-seed = int(123 + 100*np.random.rand())
-rng_key = random.PRNGKey(seed)
-rng_key, rng_key_ = random.split(rng_key)
-
-#kernel = SA(model, adapt_state_size=200)    
-kernel = NUTS(model,
-              max_tree_depth=(20, 5))
-mcmc = MCMC(kernel,
-            num_warmup=1500,
-            num_samples=6000,
-            num_chains=num_chains)  
-mcmc.run(rng_key_, extra_fields=('potential_energy',))
-pe = mcmc.get_extra_fields()['potential_energy']
-
-# extracting necessary fields for plotting
-mcmc_sample = mcmc.get_samples()
-keys = mcmc_sample.keys()
-
-# putting the true params
-refs = {}
-# initializing the keys
-for sind in range(smin_ind, smax_ind+1):
-    s = 2*sind + 1
-    for ci in range(num_params):
-        refs[f"c{s}_{ci}"] = true_params[sind-1, ci] / 1e-3
-
-ax = az.plot_pair(
-    mcmc_sample,
-    var_names=[key for key in mcmc_sample.keys()],
-    kde_kwargs={"fill_last": False},
-    kind=["scatter", "kde"],
-    marginals=True,
-    point_estimate="median",
-    figsize=(10, 8),
-    reference_values=refs,
-    reference_values_kwargs={'color':"red", "marker":"o", "markersize":6}
-)
-plt.tight_layout()
-plt.savefig('corner_reduced_prob.png')
-
-print_summary(mcmc_sample, true_params)
+print_summary(c_arr, true_params)
