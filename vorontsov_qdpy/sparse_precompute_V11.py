7203a3f490bb1b8a099cc31e6654bc1e8ab78090
srijaniiserprinceton <sbdas@princeton.edu>
2021-12-07 18:12:58 -0500

    sparse_precompute_V11 runs. Values need to be checked.

--- vorontsov_qdpy/sparse_precompute_V11.py
+++ vorontsov_qdpy/sparse_precompute_V11.py
@@ -18,10 +18,13 @@ from vorontsov_qdpy import prune_multiplets_V11
 from qdpy_jax import jax_functions as jf
 from qdpy_jax import wigner_map2 as wigmap
 from qdpy_jax import globalvars as gvar_jax
-from vorontsov_qdpy import build_cenmult_and_nbs_V11 as build_cnm
+from vorontsov_qdpy import build_cenmult_and_nbs_V11 as build_cnm_V11
+from qdpy_jax import build_cenmult_and_nbs as build_cnm
 
 # defining functions used in multiplet functions in the script
 getnt4cenmult = build_cnm.getnt4cenmult
+getnt4cenmult_V11 = build_cnm_V11.getnt4cenmult
+
 jax_minus1pow_vec = jf.jax_minus1pow_vec
 _find_idx = wigmap.find_idx
 
@@ -154,218 +157,162 @@ def build_SUBMAT_INDICES(CNM_AND_NBS):
     return SUBMAT_DICT
 
 
-def build_hm_nonint_n_fxd_1cnm(CNM_AND_NBS, SUBMAT_DICT, dim_hyper, s):
+def build_bkm_nonint_n_fxd_1cnm(CNM_AND_NBS_V11, s):
     """Computes elements in the hypermatrix excluding the
     integral part.
     """
-    # number of k's which are valid. See Vorontsov Eqn (33), (34)
-    # smax=5 can couple a max of dell = 4 (needs to be even). 
-    # (ell+k/2, ell-k/2) get coupled. So, coupling for k = 2, 4 only.
-    num_k = (GVARS.smax - 1)//2
 
     # the non-c part of b_k_m
-    noc_b_k_m = np.zeros((num_k, GVARS.nc, dim_hyper))
+    noc_b_k_m = [] 
 
     # the fixed part of b_k_m (contribution below rth)
-    fixed_b_k_m = np.zeros((num_k, dim_hyper))
+    fixed_b_k_m = []
 
     # extracting attributes from CNM_AND_NBS
-    num_nbs = len(CNM_AND_NBS.omega_nbs)
-    nl_nbs = CNM_AND_NBS.nl_nbs
+    num_nbs = len(CNM_AND_NBS_V11.omega_nbs)
+    nl_nbs = CNM_AND_NBS_V11.nl_nbs
     # omegaref = CNM_AND_NBS.omega_nbs[0]
 
-    # extracting attributes from SUBMAT_DICT
-    startx_arr, endx_arr = SUBMAT_DICT.startx_arr, SUBMAT_DICT.endx_arr
     # filling in the non-m part using the masks
-    for i in range(num_nbs):
-        # filling only Upper Triangle
-        # for j in range(i, num_nbs):
-        for j in range(num_nbs):
-            ell1, ell2 = nl_nbs[i, 1], nl_nbs[j, 1]
-            dell = ell1 - ell2
-            dellx, delly = 0, 0
-
-            if dell > 0:
-                dellx = abs(dell)
-                delly = 0
-            elif dell < 0:
-                dellx = 0
-                delly = abs(dell)
-
-            ellmin = min(ell1, ell2)
-
-            # submat tiling indices
-            startx, endx = startx_arr[i], endx_arr[i]
-            starty, endy = startx_arr[j], endx_arr[j]
-
-            wig1_idx, fac1 = _find_idx(ell1, s, ell2, 1)
-            wigidx1ij = np.searchsorted(wig_idx, wig1_idx)
-            wigval1 = fac1 * wig_list[wigidx1ij]
-
-            m_arr = np.arange(-ellmin, ellmin+1)
-            wig_idx_i, fac = _find_idx(ell1, s, ell2, m_arr)
-            wigidx_for_s = np.searchsorted(wig_idx, wig_idx_i)
-            wigvalm = fac * wig_list[wigidx_for_s]
+    # k =  +/1 or k = +/2 are arranged one after another.
+    # So, we can conveniently take steps of 2
+    for i in range(0, num_nbs, 2):
+        j = i+1   # the index for the coupling multiplet
+        
+        ell1, ell2 = nl_nbs[i, 1], nl_nbs[j, 1]
+        
+        ellmin = min(ell1, ell2)
+        
+        wig1_idx, fac1 = _find_idx(ell1, s, ell2, 1)
+        wigidx1ij = np.searchsorted(wig_idx, wig1_idx)
+        wigval1 = fac1 * wig_list[wigidx1ij]
+        
+        m_arr = np.arange(-ellmin, ellmin+1)
+        wig_idx_i, fac = _find_idx(ell1, s, ell2, m_arr)
+        wigidx_for_s = np.searchsorted(wig_idx, wig_idx_i)
+        wigvalm = fac * wig_list[wigidx_for_s]
+                
+        #-------------------------------------------------------
+        # computing the ell1, ell2 dependent factors such as
+        # gamma and Omega
+        gamma_prod = jax_gamma_(ell1) * jax_gamma_(ell2) * jax_gamma_(s) 
+        Omega_prod = jax_Omega_(ell1, 0) * jax_Omega_(ell2, 0)
+        
+        # also including 4 pi (not including 2 * omegaref as per V11)
+        ell1_ell2_fac = gamma_prod * Omega_prod *\
+                        4 * np.pi * (1 - jax_minus1pow_vec(ell1 + ell2 + s))
+        
+        # parameters for calculating the integrated part
+        eig_idx1 = nl_idx_pruned.index(CNM_AND_NBS_V11.nl_nbs_idx[i])
+        eig_idx2 = nl_idx_pruned.index(CNM_AND_NBS_V11.nl_nbs_idx[j])
+        
+        # shape (n_control_points,)
+        # integrated_part = build_integrated_part(eig_idx1, eig_idx2, ell1, ell2, s)
+        integrated_part = build_integrated_part(eig_idx1, eig_idx2, ell1, ell2, s)
+        #-------------------------------------------------------
+        # integrating wsr_fixed for the fixed part
+        fixed_integral = integrate_fixed_wsr(eig_idx1, eig_idx2, ell1, ell2, s)
+        
+        wigvalm *= (jax_minus1pow_vec(m_arr) * ell1_ell2_fac)
+        
+        wigprod = wigvalm * wigval1
+        
+        # for this particular k. Will be of shape (nc, 2*ellmin+1)
+        noc_b_m = np.zeros((GVARS.nc, 2*ellmin+1))
+        
+        for c_ind in range(GVARS.nc):
+            # non-ctrl points submat
+            # avoiding  newaxis multiplication
+            c_integral = integrated_part[c_ind] * wigprod
             
-            # the mask matrix. Setting the elements in the relevant diagonal to 1                
-            # this happens for all s where selection rule doesn't set it to zero
-            # only using the odd-even selection rule here since the triangle rule has been 
-            # taken care of in the creation of CNM and NBS
-            mask_val = np.ones_like(m_arr) * (1 - jax_minus1pow_vec(ell1 + ell2 + s))
-            mask_val = mask_val.astype('bool')
+            noc_b_m[c_ind] = c_integral
             
-            np.fill_diagonal(mask_hypmat[startx+dellx:endx-dellx,
-                                         starty+delly:endy-delly],
-                             mask_val)
-
-            #-------------------------------------------------------
-            # computing the ell1, ell2 dependent factors such as
-            # gamma and Omega
-            gamma_prod = jax_gamma_(ell1) * jax_gamma_(ell2) * jax_gamma_(s) 
-            Omega_prod = jax_Omega_(ell1, 0) * jax_Omega_(ell2, 0)
-            
-            # also including 4 pi (not including 2 * omegaref as per V11)
-            ell1_ell2_fac = gamma_prod * Omega_prod *\
-                            4 * np.pi * (1 - jax_minus1pow_vec(ell1 + ell2 + s))
-
-            # parameters for calculating the integrated part
-            eig_idx1 = nl_idx_pruned.index(CNM_AND_NBS.nl_nbs_idx[i])
-            eig_idx2 = nl_idx_pruned.index(CNM_AND_NBS.nl_nbs_idx[j])
-
-            # shape (n_control_points,)
-            # integrated_part = build_integrated_part(eig_idx1, eig_idx2, ell1, ell2, s)
-            integrated_part = build_integrated_part(eig_idx1, eig_idx2, ell1, ell2, s)
-            #-------------------------------------------------------
-            # integrating wsr_fixed for the fixed part
-            fixed_integral = integrate_fixed_wsr(eig_idx1, eig_idx2, ell1, ell2, s)
-
-            wigvalm *= (jax_minus1pow_vec(m_arr) * ell1_ell2_fac)
-
-            wigprod = wigvalm * wigval1
-            
-            for c_ind in range(GVARS.nc):
-                # non-ctrl points submat
-                # avoiding  newaxis multiplication
-                c_integral = integrated_part[c_ind] * wigprod
-                np.fill_diagonal(non_c_hypmat_arr[c_ind, startx+dellx:endx-dellx,
-                                                  starty+delly:endy-delly],
-                                 c_integral)
+        # appending to the list of k's
+        # shape (k, nc, 2*ellmin+1)
+        noc_b_k_m.append(noc_b_m)
+        
+        # the fixed hypermatrix
+        # shape (k, 2*ellmin+1)
+        f_integral = fixed_integral * wigprod
+        fixed_b_k_m.append(f_integral)
+        
     
-            # the fixed hypermatrix
-            f_integral = fixed_integral * wigprod
-            np.fill_diagonal(fixed_hypmat[startx+dellx:endx-dellx,
-                                          starty+delly:endy-delly],
-                             f_integral)
-    
-
-    return non_c_hypmat_arr, fixed_hypmat, mask_hypmat
+    return noc_b_k_m, fixed_b_k_m
 
 
-def build_hypmat_all_cenmults():
+def build_bkm_all_cenmults():
     # number of multiplets used
     nmults = len(GVARS.n0_arr)
     dim_hyper = get_dim_hyper()
 
-    # storing as a list of sparse matrices
-    # the fixed hypat (the part of hypermatrix that does not
-    # change across iterations)
-    fixed_hypmat_all_sparse = []
-    noc_hypmat_all_sparse = []
-    omegaref_nmults = []
-    ell0_nmults = []
-
-    # list to store all the sparse indices of all cenmults
-    # to be used when reconverting back to dense before eigenvlue problem
-    sp_indices_all = []
-
-
-    # getting the sparse-element size for largest ell cenmult
-    MAXMULT_AND_NBS = getnt4cenmult(GVARS.n0_arr[0], GVARS.ell0_arr[0], GVARS_ST)
-    SUBMAT_DICT_MAX = build_SUBMAT_INDICES(MAXMULT_AND_NBS)
-    __, __, maskmat_maxmult = build_hm_nonint_n_fxd_1cnm(MAXMULT_AND_NBS,
-                                                         SUBMAT_DICT_MAX,
-                                                         dim_hyper, GVARS.smax)
+    # number of k's which are valid. See Vorontsov Eqn (33), (34)
+    # smax=5 can couple a max of dell = 4 (needs to be even). 
+    # (ell+k/2, ell-k/2) get coupled. So, coupling for k = 2, 4 only.
+    num_k = (GVARS.smax - 1)//2
+    len_s = len(GVARS.s_arr)
+    # storing the arrays in dim_hyper length
+    # to facilitate easy c_l_p construction and 
+    # eigenvalue calculation later.
     
-    # finding the sp_indices_maxmult
-    maskmat_maxmult_sp = sparse.coo_matrix(maskmat_maxmult)
-    sp_indices_maxmult = (maskmat_maxmult_sp.row, maskmat_maxmult_sp.col)
+    noc_bkm_shaped = np.zeros((nmults, num_k, len_s, GVARS.nc, dim_hyper))
+    fixed_bkm_shaped = np.zeros((nmults, num_k, dim_hyper))
     
-    # the shape of all the cenmult data and indices
-    len_sp_indices_maxmult = len(sp_indices_maxmult[0])
-    
-    sp_indices_all.append(sp_indices_maxmult)
-
-    # going over the cenmults in a reverse order
-    # this is to ensure that the largest is fileld first
-    # to fill the rest in the same max shape
-    for i in tqdm(range(nmults), desc='nmult'):#-1, -1, -1):
-        # looping over all the central multiplets                                      
+    # looping over cenmtral multipelts
+    for i in range(nmults):
         n0, ell0 = GVARS.n0_arr[i], GVARS.ell0_arr[i]
-        ell0_nmults.append(ell0)
-
+        
         # building the namedtuple for the central multiplet and its neighbours            
-        CENMULT_AND_NBS = getnt4cenmult(n0, ell0, GVARS_ST)
-        SUBMAT_DICT = build_SUBMAT_INDICES(CENMULT_AND_NBS)
-        omegaref_nmults.append(CENMULT_AND_NBS.omega_nbs[0])
-
-        freqdiag = build_hypmat_freqdiag(CENMULT_AND_NBS,
-                                         SUBMAT_DICT,
-                                         dim_hyper)
+        # for V11
+        CNM_AND_NBS_V11 = getnt4cenmult_V11(n0, ell0, GVARS_ST)
         
-        noc_hypmat_this_s = []
+        # building the namedtuple for the usual supermatrix
+        # to tile the dim_hyper dimension
+        CNM_AND_NBS = getnt4cenmult(n0, ell0, GVARS_ST)
         
-        for s_ind, s in enumerate(GVARS.s_arr):
-            # shape (dim_hyper x dim_hyper) but sparse form
-            non_c_hypmat, fixed_hypmat_s, maskmat_cenmult =\
-                    build_hm_nonint_n_fxd_1cnm(CENMULT_AND_NBS,
-                                               SUBMAT_DICT,
-                                               dim_hyper, s)
-
-            noc_hypmat_sparse_c_maxshaped = np.zeros((GVARS.nc,
-                                                      len_sp_indices_maxmult))
-            for c_ind in range(GVARS.nc):
-                # the sparse data for the mask locations
-                noc_hypmat_sparse_c = non_c_hypmat[c_ind, maskmat_cenmult]
-                # enhancing the shape to maxmult
-                noc_hypmat_sparse_c_maxshaped[c_ind, :len(noc_hypmat_sparse_c)] =\
-                                                                noc_hypmat_sparse_c
-
-            # appending the different m part in the list
-            noc_hypmat_this_s.append(noc_hypmat_sparse_c_maxshaped)
+        # list of arrays for different s
+        noc_bkm_this_mult = []
+        
+        # loop in s
+        for sind, s in enumerate(GVARS.s_arr):
+        
+            # computing the unshaped bkm (noc and fixed)
+            noc_bkm, fixed_bkm =\
+                    build_bkm_nonint_n_fxd_1cnm(CNM_AND_NBS_V11, s)
             
-            # adding up the different s for the fixed part
-            if s_ind == 0:
-                fixed_hypmat_this_mult = fixed_hypmat_s
-            else:
-                fixed_hypmat_this_mult += fixed_hypmat_s
+            # to keep the start indiex of the global m dimension
+            start_global_m = int(0)
+            
+            for pind, ellp in enumerate(CNM_AND_NBS.nl_nbs[:,1]):
+                m_ellp = 2 * ellp + 1
+                
+                # global_m start and end indices depending on ellp
+                end_global_m = start_global_m + m_ellp
+                
+                # looping over k
+                for kind in range(num_k):
+                    len_m_unshaped = len(fixed_bkm[kind])
+                    
+                    # defines if there will be zero pads in the local 
+                    # m array corresponding to ellp
+                    local_dem = np.abs(m_ellp - len_m_unshaped)//2
+                    
+                    # local_m start and end indices depending on k
+                    start_local_m, end_local_m = 0, len_m_unshaped
+                    
+                    # indices to slice the shaped and unshaped m arrays
+                    if(m_ellp > len_m_unshaped):
+                        start_global_m += local_dem
+                        end_global_m -= local_dem
+                    else:
+                        start_local_m += local_dem
+                        end_local_m -= local_dem
+                        
+                        noc_bkm_shaped[i, kind, sind, :, start_global_m:end_global_m] =\
+                                    noc_bkm[kind][:, start_local_m:end_local_m]
+                        fixed_bkm_shaped[i, kind, start_global_m:end_global_m] +=\
+                                    fixed_bkm[kind][start_local_m:end_local_m]
+                        
+            # updating the start_global_m
+            start_global_m = end_global_m
 
-        # getting the mask indices
-        maskmat_cenmult_sp = sparse.coo_matrix(maskmat_cenmult)
-        sp_indices_cenmult = (maskmat_cenmult_sp.row, maskmat_cenmult_sp.col)
-
-        # adding the freqdiag to the fixed_hypmat
-        fixed_hypmat_this_mult += np.diag(freqdiag)
-        fixed_hypmat_this_mult_sparse = fixed_hypmat_this_mult[maskmat_cenmult]
-
-        # making the shape compatible to the maxmult sparse form
-        fixed_plus_freqdiag_maxshaped = np.zeros(len_sp_indices_maxmult)
-        fixed_plus_freqdiag_maxshaped[:len(fixed_hypmat_this_mult_sparse)] =\
-                                                        fixed_hypmat_this_mult_sparse
-        # appending the sparse form of the fixed hypmat
-        fixed_hypmat_all_sparse.append(fixed_plus_freqdiag_maxshaped)
-
-        # appending the list of sparse matrices in s to the list in cenmults
-        noc_hypmat_all_sparse.append(noc_hypmat_this_s)
-
-        # storing the sparse indices for the particular central multiplet
-        # if(i == nmults-1): continue
-        if(i == 0): continue
-
-        # adjusting the indices to the largest hypermatrix case (for no static slicing later)
-        sp_indices_all.append(get_sp_indices_maxshaped(sp_indices_cenmult,
-                                                       sp_indices_maxmult))
-    
-    # list of shape (nmults x s x (nc x dim_hyper, dim_hyper))
-    # the last bracket denotes matrices of that shape but in sparse form
-    return noc_hypmat_all_sparse, fixed_hypmat_all_sparse, \
-        ell0_nmults, omegaref_nmults, sp_indices_all
+        return noc_bkm_shaped, fixed_bkm_shaped
