307caad331a431a3ebe900df9ba87c2548652a46
Samarth G Kashyap <g.samarth@tifr.res.in>
2022-01-08 18:51:41 +0530

    plotting with errorbars

--- dpy_jax/run_reduced_problem_newton.py
+++ dpy_jax/run_reduced_problem_newton.py
@@ -138,15 +138,24 @@ data_acoeffs = GVARS.acoeffs_true
 print(f"data_acoeffs = {data_acoeffs[:15]}")
 
 # plotting acoeffs pred and data to see if we should expect got fit
-pred_acoeffs_plot = np.reshape(pred_acoeffs, (3,-1), 'F')
+pred_acoeffs_plot = -1*np.reshape(pred_acoeffs, (3,-1), 'F')
 data_acoeffs_plot = np.reshape(data_acoeffs, (3,-1), 'F')
+data_acoeffs_error = np.reshape(acoeffs_sigma_HMI, (3, -1), 'F')
 
 for i in range(3):
     plt.figure()
-    plt.plot(pred_acoeffs_plot[i], '.r', markersize=2)
-    plt.plot(data_acoeffs_plot[i], '.k', markersize=2)
+    plt.errorbar(np.arange(len(data_acoeffs_plot[i])),
+                 data_acoeffs_plot[i], yerr=data_acoeffs_error[i], alpha=0.5,
+                 fmt='o', ms=2, capsize=4, color='k')
+    plt.plot(pred_acoeffs_plot[i], '.r', markersize=4)
     plt.savefig(f'a{2*i+1}.png')
     plt.close()
+
+    plt.figure()
+    plt.plot(pred_acoeffs_plot[i] - data_acoeffs_plot[i], '.k', markersize=4)
+    plt.savefig(f'a{2*i+1}-res.png')
+    plt.close()
+
 
 sys.exit()
 
