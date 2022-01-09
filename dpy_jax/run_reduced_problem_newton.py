c410e64d3f2fbc6a9f3c2f503806a82bbb324a3d
srijaniiserprinceton <sbdas@princeton.edu>
2022-01-09 08:56:46 -0500

    Added proper plotting of acoeffs

--- dpy_jax/run_reduced_problem_newton.py
+++ dpy_jax/run_reduced_problem_newton.py
@@ -42,6 +42,7 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 package_dir = os.path.dirname(current_dir)
 sys.path.append(f"{package_dir}/plotter")
 import postplotter
+import plot_acoeffs_datavsmodel as plot_acoeffs
 #------------------------------------------------------------------------# 
 
 ARGS = np.loadtxt(".n0-lmin-lmax.dat")
@@ -142,24 +143,10 @@ np.testing.assert_array_almost_equal(pred_acoeffs, data_acoeffs)
 # data_acoeffs = GVARS.acoeffs_true
 print(f"data_acoeffs = {data_acoeffs[:15]}")
 
+#----------------------------------------------------------------------# 
 # plotting acoeffs pred and data to see if we should expect got fit
-pred_acoeffs_plot = -1*np.reshape(pred_acoeffs, (3,-1), 'F')
-data_acoeffs_plot = np.reshape(data_acoeffs, (3,-1), 'F')
-data_acoeffs_error = np.reshape(acoeffs_sigma_HMI, (3, -1), 'F')
-
-for i in range(3):
-    plt.figure()
-    plt.errorbar(np.arange(len(data_acoeffs_plot[i])),
-                 data_acoeffs_plot[i], yerr=data_acoeffs_error[i], alpha=0.5,
-                 fmt='o', ms=2, capsize=4, color='k')
-    plt.plot(pred_acoeffs_plot[i], '.r', markersize=4)
-    plt.savefig(f'a{2*i+1}.png')
-    plt.close()
-
-    plt.figure()
-    plt.plot(pred_acoeffs_plot[i] - data_acoeffs_plot[i], '.k', markersize=4)
-    plt.savefig(f'a{2*i+1}-res.png')
-    plt.close()
+plot_acoeffs.plot_acoeffs_datavsmodel(pred_acoeffs, data_acoeffs,
+                                      acoeffs_sigma_HMI, 'ref')
 
 #----------------------------------------------------------------------# 
 # the regularizing parameter
@@ -265,6 +252,14 @@ c_arr_renorm = jf.model_renorm(c_init*true_params_flat,
                                sigma2scale)
 c_arr_renorm = jnp.asarray(c_arr_renorm)
 
+#----------------------------------------------------------------------#                      
+# plotting acoeffs from initial data and HMI data
+init_acoeffs = data_misfit_arr_fn(c_arr_renorm)
+plot_acoeffs.plot_acoeffs_datavsmodel(init_acoeffs, data_acoeffs,
+                                      acoeffs_sigma_HMI, 'init')
+#----------------------------------------------------------------------#
+
+
 N = len(data_acoeffs)
 
 loss = 1e25
@@ -302,6 +297,14 @@ while ((abs(loss_diff) > loss_threshold) and
 
 t2s = time.time()
 print(f"Total time taken = {(t2s-t1s):12.3f} seconds")
+
+
+#----------------------------------------------------------------------#                      
+# plotting acoeffs from initial data and HMI data                                            
+final_acoeffs = data_misfit_arr_fn(c_arr_renorm)
+plot_acoeffs.plot_acoeffs_datavsmodel(final_acoeffs, data_acoeffs,
+                                      acoeffs_sigma_HMI, 'final')
+#----------------------------------------------------------------------# 
 
 # reconverting back to model_params in units of true_params_flat
 c_arr_fit = jf.model_denorm(c_arr_renorm, true_params_flat, sigma2scale)\
---
+++ plotter/plot_acoeffs_datavsmodel.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_acoeffs_datavsmodel(pred_acoeffs, data_acoeffs, data_acoeffs_sigma,
+                             label):
+    pred_acoeffs_plot = -1*np.reshape(pred_acoeffs, (3,-1), 'F')
+    data_acoeffs_plot = np.reshape(data_acoeffs, (3,-1), 'F')
+    data_acoeffs_error = np.reshape(data_acoeffs_sigma, (3, -1), 'F')
+    
+    for i in range(3):
+        plt.figure()
+        plt.errorbar(np.arange(len(data_acoeffs_plot[i])),
+                     data_acoeffs_plot[i], yerr=data_acoeffs_error[i], alpha=0.5,
+                     fmt='o', ms=2, capsize=4, color='k')
+        plt.plot(pred_acoeffs_plot[i], '.r', markersize=4)
+        plt.savefig(f'a{2*i+1}_{label}.png')
+        plt.close()
+        
+        plt.figure()
+        plt.plot(pred_acoeffs_plot[i] - data_acoeffs_plot[i], '.k', markersize=4)
+        plt.savefig(f'a{2*i+1}_res_{label}.png')
+        plt.close()
