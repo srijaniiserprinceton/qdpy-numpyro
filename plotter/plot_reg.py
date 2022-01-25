d91ac784bc8c7be6ee5edf5d192f1334eb29c110
Samarth G Kashyap <g.samarth@tifr.res.in>
2022-01-25 17:28:40 +0530

    plotting nbs of knee

--- plotter/plot_reg.py
+++ plotter/plot_reg.py
@@ -13,7 +13,7 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 package_dir = os.path.dirname(current_dir)
 data_dir = f"{package_dir}/dpy_jax"
 
-reg_data = pd.read_csv(f"{package_dir}/dpy_jax/reg_misfit.txt")
+reg_data = pd.read_csv(f"{package_dir}/dpy_jax/reg_misfit_antia.txt")
 
 mask = (reg_data['mu'] > 1e-5) * (reg_data['mu'] < 1e10)
 data_misfit = reg_data['data-misfit'][mask].values
@@ -28,14 +28,24 @@ knee_idx = np.argmin(abs(slope+1))
 range_x = data_misfit.max() - data_misfit.min()
 range_y = model_misfit.max() - model_misfit.min()
 
+max_nbs = 20
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+axs = axs.flatten()
+axs[0].plot(data_misfit, model_misfit, '+k')
+for i in range(2*max_nbs):
+    axs[0].plot(data_misfit[knee_idx-max_nbs+i],
+                model_misfit[knee_idx-max_nbs+i], '+r')
+axs[0].plot(data_misfit[knee_idx], model_misfit[knee_idx], marker='s', color='red')
+axs[0].text(data_misfit[knee_idx]+0.04*range_x, 
+            model_misfit[knee_idx]+0.04*range_y,
+            f"$\\mu$ = {mu[knee_idx]:.5f}")
+axs[0].set_xlabel('Data misfit')
+axs[0].set_ylabel('Roughness')
+axs[0].set_title('L-curve')
 
-plt.figure()
-plt.plot(data_misfit, model_misfit, '+k')
-plt.plot(data_misfit[knee_idx], model_misfit[knee_idx], marker='s', color='red')
-plt.text(data_misfit[knee_idx]+0.04*range_x, 
-         model_misfit[knee_idx]+0.04*range_y,
-         f"$\\mu$ = {mu[knee_idx]:.5f}")
-plt.xlabel('Data misfit')
-plt.ylabel('Model misfit')
-plt.title('L-curve')
-plt.show()
+axs[1].hist(mu[knee_idx-max_nbs:knee_idx+max_nbs])
+axs[1].set_xlabel('Regularization parameter $\\mu$')
+axs[1].set_ylabel('Count')
+axs[1].set_title('Distribution of $\\mu$ in the L-curve knee region')
+fig.tight_layout()
+plt.show(fig)
