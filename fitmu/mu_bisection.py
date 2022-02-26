e3feed2861a07e50c19e9a6d511afed83f73cb29
Samarth G Kashyap <g.samarth@tifr.res.in>
2022-02-26 23:55:28 +0530

    fitmu works

--- fitmu/mu_bisection.py
+++ fitmu/mu_bisection.py
@@ -1,26 +1,23 @@
 import os
 import numpy as np
 
-def get_wsr(fname):
-    c1 = np.load(fname)
-    c1full = GVARS.ctrl_arr_dpt_full * 1.0
-    c1full[2, GVARS.knot_ind_th:] = c1
-    wsr = c1full @ GVARS.bsp_basis_full
-    return c1full, wsr
-
 
 def compute_misfit(arr1, arr2):
     return np.sqrt(sum(abs(arr1 - arr2)**2))
+
 
 outdir = '/mnt/disk2/samarth/qdpy-numpyro/qdpy_iofiles/dpy_jax'
 mu_limits = [1e-15, 1e-3]
 print(f"Running python scripts")
 os.system(f"cd ../dpy_jax; python run_reduced_problem_newton.py --mu {mu_limits[0]} >temp.out 2>temp.err")
 os.system(f"cd ../dpy_jax; python run_reduced_problem_newton.py --mu {mu_limits[1]} >temp.out 2>temp.err")
-val0 = np.load(f'{outdir}/c_arr_dpt.npy')
+
+# val0 corresponds to iterative solution
+val0 = np.load(f'{outdir}/carr_iter_1.0e-6.npy')
+
+
 val1 = np.load(f'{outdir}/carr_fit_{mu_limits[0]:.5e}.npy')
 val2 = np.load(f'{outdir}/carr_fit_{mu_limits[1]:.5e}.npy')
-
 mf1 = compute_misfit(val0, val1)
 mf2 = compute_misfit(val0, val2)
 print(f"muval1 = {mu_limits[0]:.5e}; misfit = {mf1:.5e}")
