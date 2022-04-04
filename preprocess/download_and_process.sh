56533a025eebc58d9b86de7020bedd5587f36480
Samarth G Kashyap <g.samarth@tifr.res.in>
2022-04-04 16:06:01 +0530

    minor changes

--- preprocess/download_and_process.sh
+++ preprocess/download_and_process.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 INSTR=mdi
 echo "[0.] Generating the daylist ..."
-python create_daylist.py --instrument $INSTR &> temp.out
+python create_daylist.py --instrument $INSTR > temp.out 2> temp.err
 echo "[1.] Fetching data from JSOC ..."
 python fetch_data_2drls.py --instrument $INSTR
 echo "[2.] Formatting frequency data to in and out files"
